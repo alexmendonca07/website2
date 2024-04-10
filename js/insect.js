09b2c39562014c2d29ef1b6d8e909c78e33c80e4
Alex Mendonca <142828601+alexmendonca07@users.noreply.github.com>
2024-04-10 16:21:23 +0000

    Wed, Apr 10, 2024, 9:21 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -32,6 +32,7 @@ function createInsect() {
     insect.style.top = `${y}px`
     insect.style.left = `${x}px`
     insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insect.alt}" style="transform: rotate(${Math.random() * 360}deg)" />`
+    gameo_container.appendChild(insect)
 }
 
 function getRandomLocation() {
