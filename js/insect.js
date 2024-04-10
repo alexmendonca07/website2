d15ea26a7dd78b9a302d8989a8833325dda716a0
Alex Mendonca <142828601+alexmendonca07@users.noreply.github.com>
2024-04-10 15:53:33 +0000

    Wed, Apr 10, 2024, 8:53 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,5 +1,11 @@
 screens = document.querySelectorAll('.screen')
 choose_insect_btn = document.querySelectorAll('.choose-insect-btn')
-game_container = document.()getElement
-
+game_container = document.getElementById('game-container')
+start_btn = document.getElementById('start-btn')
+timeEl = document.getElementById('time')
+scoreEl = document.getElementById('score')
+message = document.getElementById('message')
+let seconds = 0
+let score = 0
+let selected_insect = {}
 
