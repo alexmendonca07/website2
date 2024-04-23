1ef03daf58a13faeef6d3b3e16d2d848091b6721
Alex Mendonca <142828601+alexmendonca07@users.noreply.github.com>
2024-04-23 16:31:55 +0000

    Tue, Apr 23, 2024, 9:31 AM -07:00

---
+++ js/hangman.js
@@ -0,0 +1,69 @@
+const wordEl = document.getElementById('word')
+const wrongLettersEl = document.getElementById('wrong-letters')
+const playAgainBtn = document.getElementById('play-again')
+const popup = document.getElementById('popup-container')
+const notification = document.getElementById('notification-container')
+const finalMessage = document.getElementById('final-message')
+const figureParts = document.querySelectorAll('.figure-part')
+
+const word = ['application', 'programming', 'interface', 'wizard']
+
+let selectedIndex = Math.floor(word.length * Math.random())
+let selectedWord = word[selectedIndex]
+
+const correctLetters = []
+const wrongLetters = []
+
+function displayWord() {
+    wordEl.innerHTML = `
+    ${selectedWord.split('').map(letter => `
+        <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+        </span>
+        ` ).join('')
+    }
+    `
+
+    const innerWord = wordEl.innerText.replace(/\n/g, '')
+
+    if(innerWord == selectedWord) {
+        finalMessage.innerText = 'Congratulations! You won!'
+        popup.style.display = 'flex'
+    }
+}
+
+function updateWrongLettersEl() {
+    // update wrong letters list
+}
+
+function showNotification() {
+    notification.classList.add('show')
+
+    setTimeout(() => {
+        notification.classList.remove('show')
+    }, 3000)
+}
+
+window.addEventListener('keydown', e => {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key
+
+        if (selectedWord.includes(letter)) {
+            if ( !correctLetters.includes(letter)) {
+                correctLetters.push(letter)
+                displayWord()
+            } else {
+                showNotification()
+            }
+        } else {
+            if(!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter)
+                updateWrongLettersEl()
+            } else {
+                showNotification()
+            }
+        }
+    }
+})
+
+displayWord()
