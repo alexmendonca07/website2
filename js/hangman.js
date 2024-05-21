4c51f9b1b3fce77cf1ae92c42fa08600be6376dc
Alex Mendonca <142828601+alexmendonca07@users.noreply.github.com>
2024-05-21 00:28:28 +0000

    Mon, May 20, 2024, 5:28 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -85,6 +85,18 @@ window.addEventListener('keydown', e => {
     }
 })
 
+// playAgainBtn.addEventListener('click', => {
+//     correctLetters.length = 0
+//     wrongLetters.length = 0
 
+//     selectedIndex = Math.floor(word.length * Math.random())
+//     selectedWord = word[selectedIndex]
+
+//     displayWord()
+
+//     updateWrongLettersEl()
+
+//     popup.style.display = 'none'
+// })
 
 displayWord()
